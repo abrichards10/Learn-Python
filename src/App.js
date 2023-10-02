f91fde4388de81960d2453ef9a21b81cd27e91f5
abrichards <angelarichards261@gmail.com>
2023-10-02 15:28:43 -0700

    editted App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from "react";
 import {
   Routes,
-  Route,
+  Route, Link,
 } from "react-router-dom";
 
 import Home from "./home";
@@ -11,18 +11,27 @@ import Comments from "./comments";
 import Print from "./print";
 import Variables from "./variables"
 import Conditionals from "./conditionals"
+import Sidebar from "./sidebar";
 function App() {
   return (
-      <>
-        <Routes >
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/setup" element={<Setup />}></Route>
-          <Route path="/comments" element={<Comments />}></Route>
-          <Route path="/print" element={<Print />}></Route>
-          <Route path="/variables" element={<Variables />}></Route>
-          <Route path="/conditionals" element={<Conditionals />}></Route>
-        </Routes>
-      </>
+      <div id="outer-container">
+        <Sidebar pageWrapId={'page-wrap'} outerContainerId={'outer-container'} />
+        <div id="page-wrap">
+          <div className="main-home">
+            <h1>Greetings</h1>
+            <p>Hello!  I’m Brooke and I am here to teach you how to code.
+              This is an ongoing project that will continue to grow
+              with time and include many more topics!  I will go over
+              them slowly - step by step, with added solutions and links
+              to videos for more explanations. Feel free to submit a form
+              link if I foolishly gave anything that is outdated or incorrect. :D
+            </p>
+            <div className="nav-buttons">
+              <Link to="/setup" className="next-button"></Link>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
