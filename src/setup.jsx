ead204cc971fba5d838d3a0a7b418cdac00b1bc3
abrichards <angelarichards261@gmail.com>
2023-10-09 14:15:59 -0700

    More setup steps

--- src/setup.jsx
+++ src/setup.jsx
@@ -13,8 +13,35 @@ class Setup extends React.Component {
                         <DarkModeToggle></DarkModeToggle>
                         <h1>Setup</h1>
                         <p>
-                            Install Python --> (Probably don't have to tbh)<br/>
-                            <a href="http://wiki.python.org/moin/BeginnersGuide/Download">Install</a>
+                            Install Python --> <i>(Probably don't have to tbh, it's usually installed)</i>
+                            <br/><a className="download-links" href="https://wiki.python.org/moin/BeginnersGuide/Download">Install</a>
+
+                            <br/> <br/>Windows? Put this in the command line:
+                            &nbsp;<div className="terminal-commands">\Users\<i>Your Name></i>python --version</div>
+
+                            <br/><br/>Mac/Linux? Put this in terminal:
+                            &nbsp;<div className="terminal-commands">python --version</div>
+                            <i>Command not found? Try: python3 --version</i>
+
+                            <br/><br/><i>Pro Tip: python / python3 <b>RUNS</b> the file</i>
+
+                            <br/><br/>You should have at least <i>one</i> of these:
+                            <br/><a className="download-links" href="https://www.jetbrains.com/idea/download/?section=mac">Intellij</a>
+                            <br/><a className="download-links" href="https://code.visualstudio.com/download">Visual Studio Code</a>
+
+                            <br/>
+                            <ul>
+                                <li>Create a new file in one of them</li>
+                                <li>Save it as helloworld.py</li>
+                                <li>Open it</li>
+                                <li>Type this:<div className="code-background-example">
+                                    print("Hello World")
+                                </div></li>
+                                <li>Run this in your Command Line/ Terminal:
+                                    &nbsp;<div className="terminal-commands">python3 helloworld.py</div>
+                                </li>
+                            </ul>
+
                         </p>
                         <div className="nav-buttons">
                             <Link to="/" className="back-button"></Link>
